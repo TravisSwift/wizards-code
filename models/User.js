c99f359cab435ad13c8c1ece77530bb5c075d835
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-12 01:59:18 -0500

    add user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+
+// create user model
+class User extends Model { }
+
+// define columns of model
+User.init(
+    {
+        id: {
+            // a required number as primary key, autoincremented to prevent duplicates
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            // users can choose any username they like, but must have one
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            // only one user per email, must have email
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            // must have a pw of min 6 chars
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        }
+    }
+)
+
+module.exports = User;
