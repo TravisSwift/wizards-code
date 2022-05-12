3905ad8bb863b31ae00a1437b24a23e893991783
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-12 01:51:31 -0500

    added majority of info for character model

--- __tests__/Character.test.js
+++ __tests__/Character.test.js
@@ -1,3 +1,13 @@
 const Character = require('../models/Character');
 
-// test('characters something something')
+test('creates a character with 6 stats & modifiers', () => {
+    const char = new Character('Emil');
+
+    expect(char.name).toBe('Emil');
+    expect(char.strength).toEqual(expect.any(Number));
+    expect(char.dexterity).toEqual(expect.any(Number));
+    expect(char.constitution).toEqual(expect.any(Number));
+    expect(char.intelligence).toEqual(expect.any(Number));
+    expect(char.wisdom).toEqual(expect.any(Number));
+    expect(char.charisma).toEqual(expect.any(Number));
+})
