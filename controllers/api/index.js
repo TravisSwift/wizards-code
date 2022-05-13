fdc823b930941a2cc608ed2d8418132a2cf4a2dd
sacylkowski <sacylkowski@gmail.com>
2022-05-12 19:39:52 -0500

    index routes updated with model routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 
-// const userRoutes = require("./user-routes.js");
-// const postRoutes = require("./post-routes.js");
-// const commentRoutes = require("./comment-routes.js");
+const userRoutes = require("./user-routes.js");
+const characterRoutes = require("./character-routes.js");
 
-// router.use("/users", userRoutes);
-// router.use("/posts", postRoutes);
-// router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
+router.use("/characters", characterRoutes);
 
 module.exports = router;
