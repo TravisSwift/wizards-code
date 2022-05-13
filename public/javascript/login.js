6c6c63581f78a57bfe8c54bc76751eb2be77cea9
sacylkowski <sacylkowski@gmail.com>
2022-05-13 13:28:32 -0500

    fixed login code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,29 @@
+async function loginForm(event) {
+    event.preventDefault();
+
+    // grabbing the data from the form
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
+
+    // if all information is provided
+    if (email && password) {
+        const response = await fetch("/api/users/login", {
+            method: "post",
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { "Content-Type": "application/json" }
+         });
+         if (response.ok) {
+             document.location.replace("/");
+         } else {
+             alert(response.statusText);
+         }
+    }
+}
+
+
 async function signupForm(event) {
     event.preventDefault();
 
@@ -18,31 +44,6 @@ async function signupForm(event) {
             headers: { "Content-Type": "application/json" }
          });
          if (response.ok) {
-             console.log("success");
-         } else {
-             alert(response.statusText);
-         }
-    }
-}
-
-async function loginForm(event) {
-    event.preventDefault();
-
-    // grabbing the data from the form
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-
-    // if all information is provided
-    if (email && password) {
-        const response = await fetch("/api/users/login", {
-            method: "post",
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { "Content-Type": "application/json" }
-         });
-         if (response.ok) {
              document.location.replace("/");
          } else {
              alert(response.statusText);
@@ -50,5 +51,6 @@ async function loginForm(event) {
     }
 }
 
+document.querySelector(".login-form").addEventListener("submit", loginForm);
+
 document.querySelector(".signup-form").addEventListener("submit", signupForm);
-document.querySelector(".login-form").addEventListener("submit", signupForm);
