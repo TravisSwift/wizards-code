84b37895bafe325b312f819e9cead8c73fa4950d
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-12 19:38:34 -0500

    added model relationships

--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
 const Character = require('./Character');
 const User = require('./User');
+
+// create associations
+User.hasMany(Character, {
+    foreignKey: 'user_id'
+});
+
+Character.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Character };
