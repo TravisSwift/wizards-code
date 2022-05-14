c5e558662180d14c20393e6c1ea72fb7c877e9fe
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-14 10:41:09 -0500

    changed skills and proficiency to be DRY

--- public/javascript/skills.js
+++ public/javascript/skills.js
@@ -1,11 +1,6 @@
-// use proficiency and stat modifiers to determine skill bonus
-const getSkill = (prof, stat) => {
-    return prof + stat;
-}
-
 // calculate saving throw based on whether save is proficient or not
-const getSave = (save, prof, stat) => {
-    if (save.isProficient) {
+const getSaveorSkill = (saveSkill, prof, stat) => {
+    if (saveSkill.isProficient) {
         return stat + prof;
     } else {
         return stat;
