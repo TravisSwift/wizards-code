be2ef8d5e3bb5eb640bf71b4e035d6633fa8e432
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-17 20:12:49 -0500

    started load functionality

---
+++ public/javascript/load.js
@@ -0,0 +1,14 @@
+// html ids
+const loadBtn = document.getElementById("load-character");
+
+// load button handler (receive character)
+const loadBtnHandler = (event) => {
+    event.preventDefault();
+
+    // grab character id
+}
+
+// put character info in sheet
+
+// event listener
+loadBtn.addEventListener("click", loadBtnHandler);
